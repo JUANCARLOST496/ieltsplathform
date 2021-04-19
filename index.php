2d7f2868c1a9125bef15fd608e7f269ac2b1b94b
juan <juancarlost@gmail.com>
2021-04-19 16:25:43 -0500

    datos realizados

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php include 'inc/layout/header.php';?>
 
+<div class="contenedor-barra">
+        <h1>Agenda de contactos
+        </h1>
+        </div>
+<div class="bg-amarillo contenedor sombra">
+         <form id="contacto" action="#">
+           <legend>Add un campo<span> todo los campos son obligatorios</span>
+           </legend>
+           <div class="campos">
+                
+                
+                
+                        <div class="campo">
+                        <label for='nombre'>Nombre</label>
+                        <input type="text" placeholder="Nombre producto" id="nombre">
+                        </div>
+                        <div class="campo">
+                        <label for='empresa'>Empresa</label>
+                        <input type="text" placeholder="Nombre empresa" id="empresa">
+                        </div>
+                        <div class="campo">
+                        <label for='telefono'>tel</label>
+                        <input type="tel" placeholder="Nombre contacto" id="telefono">
+                        </div>
+                        <div class="campo-enviar">
+                        <input type="submit" value="añadir">
+                        </div>
+              </div>
+         </form>
+        
+</div>
 
 
 

0a52ce373d6fe503692469c74b2d3644ca13bef5
juan <juancarlost@gmail.com>
2021-04-24 22:59:15 -0500

    se actualiza la base de datos

--- INC/funciones/bd.php
+++ INC/funciones/bd.php
@@ -1,10 +1,9 @@
 <?php 
 // Credenciales
-define('BD_USUARIO','root');
+define('DB_USUARIO','root');
 define('DB_PASSWORD','work55.55T');
 define('DB_HOST','localhost');
-define('DB_NOMBRE','agendaphp');
+define('DB_NOMBRE','negocio');
 
 $conn =new mysqli(DB_HOST,DB_USUARIO,DB_PASSWORD,DB_NOMBRE);
 
-echo $conn->ping();
--- INC/modelos/mcon.php
+++ INC/modelos/mcon.php
@@ -1,3 +1,23 @@
 <?php
 
-echo json_encode($_POST);
+
+if($_POST['accion']=='crear'){
+     require_once('../funciones/bd.php');
+
+    //validar entradas
+   $nombre=filter_var($_POST['nombre'],FILTER_SANITIZE_STRING);
+   $nombre=filter_var($_POST['empresa'],FILTER_SANITIZE_STRING);
+   $nombre=filter_var($_POST['telefono'],FILTER_SANITIZE_STRING);
+
+   try{
+    $statement =$conn->prepare("INSERT INTO negocio VALUES (?,?,?)");
+   }catch(Exception $e){
+    $respuesta = array (
+         'error'=> $e->getMessage()
+     );
+    }
+   
+
+     echo json_encode($_POST);
+   
+}

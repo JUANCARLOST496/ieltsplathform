6fade3e6db2fd1e3528707df5100268c52877711
juan <juancarlost@gmail.com>
2021-04-28 23:49:37 -0500

    paginalogin

---
+++ Login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Document</title>
+
+</head>
+<body>
+<div class="row">
+<div class="col s12 l4 offset-l4">
+             <div class="card">
+                        <div class="card-action red white-text center-align">
+                              <h3>USUARIO</h3>
+                        </div>
+                        <div class="card-content">
+                          <div class="form-field">
+                                <label for="username">Username</label>
+                                <input type="text" id="username">
+                          </div><br>
+                          <div class="form-field">
+                                <label for="username">Password</label>
+                                <input type="text" id="username">
+                          </div><br>
+                          <div class="form-field">
+                                <button>Loguin</button>
+                          </div><br>
+                        </div>
+                        
+                        </div>
+
+
+             </div>
+
+</div>
+
+
+</div>
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
+
+
+
+
